a7b9763c113f60d63896a79c4a65447118973da0
Alok Dwivedi <alokdwivedi103@gmail.com>
2023-04-11 11:21:50 +0530

    Changed according to the comments

--- src/Form.js
+++ src/Form.js
@@ -1,75 +1,38 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
+import { validateFormData } from './errorutil'
 import "./Form.css";
+
+const defaultForm = {
+  loginId: "",
+  email: "",
+  name: "",
+  timezone: "",
+  homePage: "",
+  aboutMe: "",
+  receiveNotifications: false
+}
+
 const Form = () => {
-  const [formData, setFormData] = useState({
-    loginId: "",
-    email: "",
-    name: "",
-    timezone: "",
-    homePage: "",
-    aboutMe: "",
-    receiveNotifications: false
-  });
-  const selectRef = useRef(null);
+  const [formData, setFormData] = useState(defaultForm);
   const [errors, setErrors] = useState({});
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
+    const { name, value } = e.target
     setFormData({ ...formData, [name]: value });
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+
     const validationErrors = validateFormData(formData);
     if (Object.keys(validationErrors).length > 0) {
+      e.preventDefault();
       setErrors(validationErrors);
     } else {
-      setFormData({
-        loginId: "",
-        email: "",
-        name: "",
-        timezone: "",
-        homePage: "",
-        aboutMe: "",
-        receiveNotifications: false
-      });
+      setFormData({ defaultForm });
       setErrors({});
     }
   };
-  const validateFormData = (formData) => {
-    const errors = {};
 
-    if (!formData.loginId) {
-      errors.loginId = "Login ID is required";
-    }
-    if (!formData.email) {
-      errors.email = "Email is required";
-    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
-      errors.email = "Invalid email format";
-    }
-    if (!formData.name) {
-      errors.name = "Name is required";
-    }
-    if (document.querySelector("select").value === "") {
-      errors.timezone = "Timezone is required";
-    }
-    if (!formData.homePage) {
-      errors.homePage = "Home page is required";
-    } else if (
-      !/^(http|https):\/\/[a-zA-Z0-9]+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,20}(:[0-9]{1,5})?(\/.*)?$/.test(
-        formData.homePage
-      )
-    ) {
-      errors.homePage = "Invalid URL format";
-    }
-    if (formData.aboutMe.length > 0 && formData.aboutMe.length < 50) {
-      errors.aboutMe = "About Me must be at least 50 characters";
-    }
-    if (!formData.receiveNotifications) {
-      errors.receiveNotifications = "You must accept to receive notifications";
-    }
-    return errors;
-  };
 
   return (
     <>
@@ -86,10 +49,10 @@ const Form = () => {
             value={formData.loginId}
             onChange={handleChange}
           />
-          
+
         </div>
         <br />
-          {errors.loginId && <span>{errors.loginId}</span>}
+        {errors.loginId && <span>{errors.loginId}</span>}
         <div className="Form_entry">
           <label htmlFor="email">Email</label>
           <input
@@ -99,10 +62,10 @@ const Form = () => {
             value={formData.email}
             onChange={handleChange}
           />
-          
+
         </div>
         <br />
-          {errors.email && <span>{errors.email}</span>}
+        {errors.email && <span>{errors.email}</span>}
         <div className="Form_entry">
           <label htmlFor="name">Name</label>
           <input
@@ -112,30 +75,30 @@ const Form = () => {
             value={formData.name}
             onChange={handleChange}
           />
-          
+
         </div>
         <br />
-          {errors.name && <span>{errors.name}</span>}
+        {errors.name && <span>{errors.name}</span>}
         <div className="Form_entry">
           <label htmlFor="timezone">Timezone</label>
-          <select id="Timezone">
+          <select id="Timezone" onChange={handleChange}>
             <option value="" disabled selected>
               Select a timezone
             </option>
-            <option value="GMT" ref={selectRef}>
+            <option value="GMT">
               GMT
             </option>
-            <option value="IST" ref={selectRef}>
+            <option value="IST">
               IST
             </option>
-            <option value="DST" ref={selectRef}>
+            <option value="DST">
               DST
             </option>
           </select>
-          
+
         </div>
         <br />
-          {errors.timezone && <span>{errors.timezone}</span>}
+        {errors.timezone && <span>{errors.timezone}</span>}
         <div className="Form_entry">
           <label htmlFor="homePage">Home Page</label>
           <input
@@ -145,10 +108,10 @@ const Form = () => {
             value={formData.homePage}
             onChange={handleChange}
           />
-          
+
         </div>
         <br />
-          {errors.homePage && <span>{errors.homePage}</span>}
+        {errors.homePage && <span>{errors.homePage}</span>}
         <div className="Form_entry">
           <label htmlFor="aboutMe">About Me</label>
 
@@ -158,10 +121,10 @@ const Form = () => {
             value={formData.aboutMe}
             onChange={handleChange}
           />
-          
+
         </div>
         <br />
-          {errors.aboutMe && <span>{errors.aboutMe}</span>}
+        {errors.aboutMe && <span>{errors.aboutMe}</span>}
         <div className="checkbox">
           <label htmlFor="receiveNotifications">
             <input
@@ -169,12 +132,7 @@ const Form = () => {
               id="receiveNotifications"
               name="receiveNotifications"
               checked={formData.receiveNotifications}
-              onChange={(e) =>
-                setFormData({
-                  ...formData,
-                  receiveNotifications: e.target.checked
-                })
-              }
+              onChange={handleChange}
             />
             Receive Notifications of comments.
             <br />
